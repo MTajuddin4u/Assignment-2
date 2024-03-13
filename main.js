a72c0c1412251626f0601b391d88a2e7acc33801
MTajuddin4u <159999265+MTajuddin4u@users.noreply.github.com>
2024-03-13 17:51:07 +0500

    These are my second three files

---
+++ main.js
@@ -0,0 +1,18 @@
+"use strict";
+console.log("hello world");
+console.log("hello welcome back to my channel");
+console.log("AI Planet");
+console.log("This is the third class of IT");
+console.log("Thank you governor Kamran Khan Tessori for giving us this opportunity");
+//variables are like containers that store values, datatypes.
+//we make variables with 3 types
+//let
+//var "we will not use var because it don't detect errors"
+//const "we will use cont when someting is unchangeable"
+//comments "when we don't want to run/execute these lines"
+//let message="hello world"
+//console.log(message);
+let message="hello world"
+message="Hello AI Planet"
+console.log(message);
+
---
+++ main.ts
@@ -0,0 +1,5 @@
+console.log("hello world");
+console.log("hello welcome back to my channel");
+console.log("AI Planet");
+console.log("This is the third class of IT");
+console.log("Thank you governor Kamran Khan Tessori for giving us this opportunity");
